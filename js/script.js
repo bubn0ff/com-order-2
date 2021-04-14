abc4df5c73dc59d5d52fb3b9f6ac310a9e7bea78
Oleg Bubnov <bubn0ff@ya.ru>
2021-04-14 10:39:09 +0300

    Upd: added sript.js and changed iframe

---
+++ js/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+  setTimeout(function() {
+    document.getElementByClass('video__iframe').src = 'https://www.youtube.com/embed/jKNdGbyrzZg';
+  }, 5000);
+});
