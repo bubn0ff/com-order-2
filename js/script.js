7cf78a63fca018c0f45f9ec66f5628a29ee8b1d2
Oleg Bubnov <bubn0ff@ya.ru>
2021-04-14 10:54:54 +0300

    Fix: fixed error in script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function() {
   setTimeout(function() {
-    document.getElementByClass('video__iframe').src = 'https://www.youtube.com/embed/jKNdGbyrzZg';
+    document.querySelector('.video__iframe').src = 'https://www.youtube.com/embed/jKNdGbyrzZg';
   }, 3000);
 });
